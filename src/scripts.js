d527c9c93e7a13e6a8cc82f8c00c17b4fea7879a
Jessica Fatta <jessicafatta@gmail.com>
2022-03-03 14:09:59 -0500

    Beautify page

--- src/scripts.js
+++ src/scripts.js
@@ -1,4 +1,4 @@
-
+//IMPORTS
 import './css/styles.css';
 import datepicker from 'js-datepicker';
 import dayjs from 'dayjs'
@@ -6,25 +6,26 @@ import dayjs from 'dayjs'
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import './images/airplane.png';
 
-
-
-
+//GLOBALS
+//let dayjs;
+let daypicker;
 
 
 
 // query selectors
 const calendar = document.querySelector('#calendar')
+const welcomeName = document.querySelector('#welcomeName')
 
-// const datePicker = datepicker('#calendar', {
-//   onSelect: (instance, date) => {
-//     loadHydrationCard(currentUser)
-//     hydrationChart.destroy()
-//     displayHydrationChart()
-//     loadSleepCard(currentUser)
-//     sleepChart.destroy()
-//     displaySleepChart()
-//   },
-//   startDate: new Date(2019, 5, 22),
-//   minDate: new Date(2019, 5, 15),
-//   maxDate: new Date(2020, 0, 22)
-// })
+const datePicker = datepicker('#calendar', {
+  onSelect: (instance, date) => {
+    //loadHydrationCard(currentUser)
+    //hydrationChart.destroy()
+    //displayHydrationChart()
+    //loadSleepCard(currentUser)
+    //sleepChart.destroy()
+    //displaySleepChart()
+  },
+  startDate: new Date(2019, 7, 2),
+  minDate: new Date(2019, 7, 2),
+  maxDate: new Date(2022, 12, 19)
+})
