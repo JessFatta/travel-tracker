d2de10145e6cb54bb0252dd6dfdceb50201150b1
Jessica Fatta <jessicafatta@gmail.com>
2022-03-03 15:55:53 -0500

    Update before each to incoporate sample test data

--- test/Traveler-test.js
+++ test/Traveler-test.js
@@ -1,18 +1,20 @@
 import { expect } from 'chai'
+import { travelerTestData } from './test-data'
 import Traveler from '../src/Traveler'
 
 describe('Traveler', () => {
-  let traveler;
-  let travelerData;
+  //let traveler;
+  //let travelerData;
+  let traveler1;
+  let traveler2;
+  let traveler3;
   
   beforeEach(() => {
-    travelerData = {
-      id: 1,
-      name: 'Ham Leadbeater',
-      travelerType: 'relaxer'
-    }
 
-    traveler = new Traveler(travelerData)
+
+    traveler1 = new Traveler(travelerTestData[0]);
+    traveler2 = new Traveler(travelerTestData[1]);
+    traveler3 = new Traveler(travelerTestData[2]);
   })
 
   it('should be a function', () => {
@@ -20,23 +22,36 @@ describe('Traveler', () => {
   })
 
   it('should instaniate the Traveler class', () => {
-    expect(traveler).to.be.an.instanceof(Traveler)
+    expect(traveler1).to.be.an.instanceof(Traveler)
+    expect(traveler2).to.be.an.instanceof(Traveler)
+    expect(traveler3).to.be.an.instanceof(Traveler)
+
   })
 
   it('should have an id', () => {
-    expect(traveler.id).to.equal(1)
+    expect(traveler1.id).to.equal(1)
   })
 
   it('should have a name', () => {
-    expect(traveler.name).to.equal('Ham Leadbeater')
+    expect(traveler1.name).to.equal('Ham Leadbeater')
   })
 
   it('should have a traveler type', () => {
-    expect(traveler.travelerType).to.equal('relaxer')
+    expect(traveler1.travelerType).to.equal('relaxer')
   })
 
-  it('should have a method to calculate total amount spent on trips and include a 10% agent fee', () => {
-    expect(traveler.totalAmountSpent())
-  })
-  //maybe it should store an array of trips?
+  // it('should have a method to calculate total amount spent on trips and include a 10% agent fee', () => {
+  //   expect(traveler.getYearlyAmountSpent())
+  // })
+
+  // it('should be able to store all trips for a user', () => {
+  //   //expect(traveler.addTripsForCurrentUser())
+  //   //maybe it should store an array of trips?
+  // })
+
+  //should be able to see past, current, upcoming, and pending trips
+  //should be able to make a trip request
+  //should be able to select a date, duration, num travelers, select destination
+  //should see estimated cost with 10% fee after making a selection
+  //should see a pending response after submitting trip request
 })
