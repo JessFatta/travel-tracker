b6ec04b442142492b1f095d15b89301c07f78dd0
Jessica Fatta <jessicafatta@gmail.com>
2022-03-02 20:45:02 -0500

    Initial build of css

--- src/scripts.js
+++ src/scripts.js
@@ -1,11 +1,30 @@
-// This is the JavaScript entry file - your code begins here
-// Do not delete or rename this file ********
 
-// An example of how you tell webpack to use a CSS (SCSS) file
-import './css/base.scss';
+import './css/styles.css';
+import datepicker from 'js-datepicker';
+import dayjs from 'dayjs'
 
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import './images/turing-logo.png'
 
 
-console.log('This is the JavaScript entry file - your code begins here.');
+
+
+
+
+
+// query selectors
+const calendar = document.querySelector('#calendar')
+
+// const datePicker = datepicker('#calendar', {
+//   onSelect: (instance, date) => {
+//     loadHydrationCard(currentUser)
+//     hydrationChart.destroy()
+//     displayHydrationChart()
+//     loadSleepCard(currentUser)
+//     sleepChart.destroy()
+//     displaySleepChart()
+//   },
+//   startDate: new Date(2019, 5, 22),
+//   minDate: new Date(2019, 5, 15),
+//   maxDate: new Date(2020, 0, 22)
+// })
