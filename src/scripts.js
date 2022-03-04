a6ef32f71e8663cefda9a6e78333e984f56a6bf6
Jessica Fatta <jessicafatta@gmail.com>
2022-03-03 21:45:14 -0500

    Add some parse functions

--- src/scripts.js
+++ src/scripts.js
@@ -1,20 +1,31 @@
-//IMPORTS
+//------IMPORTS
 import './css/styles.css';
+import './images/airplane.png';
+
 import datepicker from 'js-datepicker';
 import dayjs from 'dayjs';
 
+import Traveler from './Traveler'
+import Destination from './Destination';
+import Trip from './Trip'
+
+import {
+  fetchTravelerData, 
+  fetchDestinationData, 
+  fetchTripData
+} from './apiCalls'
 
 
-// An example of how you tell webpack to use an image (also need to link to it in the index.html)
-import './images/airplane.png';
-
-//GLOBALS
+//------GLOBALS
 //let dayjs;
 let daypicker;
+let travelers;
+let allTravelers;
+let allTrips;
+let allDestinations;
 
 
-
-// query selectors
+//------QUERY SELECTORS
 const calendar = document.querySelector('#calendar')
 const welcomeName = document.querySelector('#welcomeName')
 
@@ -31,3 +42,66 @@ const datePicker = datepicker('#calendar', {
   minDate: new Date(2019, 7, 2),
   maxDate: new Date(2022, 12, 19)
 })
+
+
+//------FUNCTIONS
+const fetchAllData = () => {
+  Promise.all([
+    fetchTravelerData(),
+    fetchDestinationData(),
+    fetchTripData()
+  ]).then((data) => parseAllData(data))
+}
+
+
+// const getRandomIndex = (array) => {
+//   return Math.floor(Math.random() * array.length)
+// }
+
+
+// const selectRandomTraveler = () => {
+//   const randomIndex = getRandomIndex(travelers)
+//   return travelers[randomIndex]
+// }
+
+
+
+// const displayRandomTraveler = () => {
+//   let currentTraveler = selectRandomTraveler()
+//   displayTravelerName(currentTraveler)
+// }
+
+// displayRandomTraveler()
+
+const parseAllData = (data) => {
+  allTravelers = data[0].travelers.map(traveler => new Traveler(traveler))
+  //parseTravelerData(allTravelers)
+  allTrips = data[2].trips.map(trip => new Trip(trip))
+  allDestinations = data[1].destinations.map(destination => new Destination(destination))
+  //displayTravelerName()
+}
+
+// const parseTravelerData = (travelerData) => {
+//   const filteredTravelerData = {}
+//   travelerData.forEach()
+  
+// }
+
+
+
+
+
+
+
+
+
+const displayTravelerName = (travelerData) => {
+  welcomeName.innerText = `Welcome, ${allTravelers[0].name}`
+}
+
+
+
+
+
+//------EVENT LISTENERS
+window.addEventListener('load', fetchAllData)
