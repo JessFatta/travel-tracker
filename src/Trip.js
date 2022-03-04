55206370844fe4e30801bbfeba08cc2369f30c8a
Jessica Fatta <jessicafatta@gmail.com>
2022-03-03 21:44:06 -0500

    Add trip method

--- src/Trip.js
+++ src/Trip.js
@@ -1,3 +1,5 @@
+import Destination from "./Destination";
+
 class Trip {
   constructor(trip) {
     this.id = trip.id;
@@ -8,6 +10,33 @@ class Trip {
     this.duration = trip.duration;
     this.status = trip.status;
     this.suggestedActivities = trip.suggestedActivities
+  }
+
+
+  getDestinationName(destinationTestData) {
+    const destination = destinationTestData.find(destination => {
+      if(destination.id === this.destinationID) {
+        this.destinationName = destination.destination
+        this.destinationImage = destination.destinationImage
+        this.destinationAlt = destination.alt
+      }
+    })
+    return destination
+  }
+
+  calculateTripCost(destinationTestData) {
+    let tripCost = 0;
+    let agentCharge = .1;
+    destinationTestData.forEach(destination => {
+      if(this.destinationID === destination.id) {
+        const flightsCost = destination.estimatedFlightCostPerPerson * this.travelers
+        const lodgingCost = destination.estimatedLodgingCostPerDay * this.duration * this.travelers
+        tripCost += flightsCost
+        tripCost += lodgingCost
+      }
+    })
+    let totalWithFee = tripCost * agentCharge 
+    return totalWithFee + tripCost
   }
 }
 
