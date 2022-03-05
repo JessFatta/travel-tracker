f7f663fee315fc1d775b5c94d464a2998c1f1465
Jessica Fatta <jessicafatta@gmail.com>
2022-03-04 20:26:26 -0500

    Initial test of displaying trips

--- src/scripts.js
+++ src/scripts.js
@@ -14,6 +14,8 @@ import {
   fetchDestinationData, 
   fetchTripData
 } from './apiCalls'
+import { all } from 'core-js/fn/promise';
+import DataRepo from './DataRepo';
 
 
 //------GLOBALS
@@ -22,7 +24,9 @@ let daypicker;
 let travelers;
 let trips;
 let destinations;
-
+let randomIndex;
+let allData = {}
+let dataRepo
 
 // let allTravelers;
 // let allTrips;
@@ -32,6 +36,7 @@ let destinations;
 //------QUERY SELECTORS
 const calendar = document.querySelector('#calendar')
 const welcomeName = document.querySelector('#welcomeName')
+const presentTripsBox = document.querySelector('.present-trips-box')
 
 const datePicker = datepicker('#calendar', {
   onSelect: (instance, date) => {
@@ -54,14 +59,16 @@ const fetchAllData = () => {
 
 
 const parseAllData = (data) => {
-  travelers = data[0].travelers.map(traveler => new Traveler(traveler))
+  const dataRepo = {}
 
-  //parseTravelerData(allTravelers)
-  trips = data[2].trips.map(trip => new Trip(trip))
+
+  dataRepo.travelers = data[0].travelers.map(traveler => new Traveler(traveler))
+  dataRepo.trips = data[2].trips.map(trip => new Trip(trip))
+  dataRepo.destinations = data[1].destinations.map(destination => new Destination(destination))
   
-  destinations = data[1].destinations.map(destination => new Destination(destination))
-  
-  getRandomTraveler(travelers)
+  allData = new DataRepo(dataRepo)
+  //console.log(allData)
+  getRandomTraveler(allData.travelers)
 
 }
 
@@ -72,15 +79,31 @@ const parseAllData = (data) => {
   
   // }
 
-  const getRandomTraveler = (array) => {
-    randomIndex = Math.floor(Math.random() * array.length)
-    displayTravelerName(randomIndex)
-  }
-  
-let randomIndex
+const getRandomTraveler = (array) => {
+  randomIndex = Math.floor(Math.random() * array.length)
+  displayTravelerName(allData.randomIndex)
+}
+
 
 const displayTravelerName = (traveler) => {
-  welcomeName.innerText = `Welcome, ${travelers[randomIndex].getFirstName()}`
+  welcomeName.innerText = `Welcome, ${allData.travelers[randomIndex].getFirstName()}`
+  displayTrips()
+}
+
+const displayTrips = () => {
+  const userTrips = allData.getTravelerTrips(randomIndex + 1)
+  console.log(userTrips)
+
+  userTrips.forEach(trip => {
+    let currentDestination = allData.destinations.find(location => trip.destinationID === location.id)
+    console.log(currentDestination)
+
+    presentTripsBox.innerHTML += `
+      <p class="destination-name">${currentDestination.destination}</p><br>
+      <img class="destination-image" src="${currentDestination.image}" alt="${currentDestination.alt}"/><br>
+      <p class="destination-info">Destination info</p>
+    `
+  })
 }
 
 
