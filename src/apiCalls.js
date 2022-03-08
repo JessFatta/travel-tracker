1e8d7104f79010da2731a93eba3029428e02d7f4
Jessica Fatta <jessicafatta@gmail.com>
2022-03-08 13:21:16 -0500

    Add post

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -17,6 +17,24 @@ const fetchTripData = () => {
   .catch((error) => console.log(error))
 }
 
+const postNewTrip = (trip) => {
+  return fetch('http://localhost:3001/api/v1/trips', {
+    method: "POST",
+    headers: { "Content-Type": "application/json"},
+    body: JSON.stringify(trip),
+  })
+  .then((response) => response.json())
+}
+
+// const errorHandling = (response) => {
+//   if(!response.ok) {
+//     throw 'We are having some trouble completing your request'
+//   } else {
+//     pendingTripText.innerText = 'Trip Successfully Submitted'
+//     response.json()
+//   }
+//   }
 
 
-export { fetchTravelerData, fetchDestinationData, fetchTripData }
+
+export { fetchTravelerData, fetchDestinationData, fetchTripData, postNewTrip }
