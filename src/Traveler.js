5e1e0c5d1d8aa28ce4a9e5469a375144195910fe
Jessica Fatta <jessicafatta@gmail.com>
2022-03-07 21:07:15 -0500

    Update traveler

--- src/Traveler.js
+++ src/Traveler.js
@@ -6,11 +6,12 @@ class Traveler {
     this.id = travelerData.id;
     this.name = travelerData.name;
     this.travelerType = travelerData.travelerType
-    this.allTrips ;
-    this.pastTrips = [];
-    this.presentTrips = [];
-    this.upcomingTrips = [];
-    this.pendingTrips = [];
+    this.allTrips;
+    // this.pastTrips = [];
+    // this.travelersTrips = []
+    // this.presentTrips = [];
+    // this.upcomingTrips = [];
+    // this.pendingTrips = [];
   }
 
   getFirstName() {
